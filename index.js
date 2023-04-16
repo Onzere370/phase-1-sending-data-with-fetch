a1e78bb843498bceb9f36d04f2b9e75a804d6263
cynthia <cynthiakagz@gmail.com>
2023-04-16 17:07:07 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+
+const submitData = {
+    userName:"",
+    userEmail:""
+
+  };
+  
+  const userObj = {
+    method:'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(submitData),
+  };
+  
+  fetch('http://localhost:3000/users', userObj)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (userObj) {
+      console.log(userObj);
+    })
+    .catch(error => console.log('Error submitting data:', error));
+    
